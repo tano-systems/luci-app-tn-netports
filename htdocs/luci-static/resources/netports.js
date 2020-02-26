tano-systems/luci-app-tn-netports

commit 41cf995e919f7840b27246af7c3ef0e7a79b2f1c

Sync firewall zones colors
In recent versions of LuCI the colors of the firewall zones are
generated by JavaScript code depending on the network name.
In netports code, the zone colors are generated by the old Lua
code from the luci-compat package. As a result, the colors of zones
in the netports table and LuCI network settings are different.

This commit fixes this problem in a such a way that a new JavaScript
code is used to get the colors of the firewall zones.

Signed-off-by: Anton Kikin <a.kikin@tano-systems.com>

htdocs/luci-static/resources/netports.js
@@ -6,6 +6,7 @@
 'use strict';
 'require ui';
 'require uci';
+'require firewall';
 
 const NetPortsMode = {
 	H: 0,
@@ -198,7 +199,7 @@ var NetPorts = L.Class.extend({
 				var ifname = '';
 
 				z += '<div class="ifacebox netports-ifacebox">';
-				z += '<div class="ifacebox-head netports-ifacebox-head" style="background-color: ' + n.fwzone_color + ';">';
+				z += '<div class="ifacebox-head netports-ifacebox-head" style="background-color: ' + firewall.getColorForName(n.netname) + ';">';
 
 				if (out_ifname)
 					ifname = n.netname.toUpperCase() + ': ';

root/usr/libexec/rpcd/netports
@@ -124,10 +124,7 @@ local methods = {
 					l[name]["wifiname"] = wifiname
 					if fwzone then
 						l[name]["fwzone"]       = fwzone:name()
-						l[name]["fwzone_color"] = fwzone:get_color()
 						l[name]["fwzone_sid"]   = fwzone.sid
-					else
-						l[name]["fwzone_color"] = "#EEEEEE"
 					end
 				end
 			end

commit fccb08931ab2d7a7abd8180116bf5b19eda7660a

Fix firewall zones colors
To determine the color of the firewall zone, use the zone
name, not the network interface.

Fixes: 41cf995 ("Sync firewall zones colors")
Signed-off-by: Anton Kikin <a.kikin@tano-systems.com>

htdocs/luci-static/resources/netports.js
@@ -199,7 +199,7 @@ var NetPorts = L.Class.extend({
 				var ifname = '';
 
 				z += '<div class="ifacebox netports-ifacebox">';
-				z += '<div class="ifacebox-head netports-ifacebox-head" style="background-color: ' + firewall.getColorForName(n.netname) + ';">';
+				z += '<div class="ifacebox-head netports-ifacebox-head" style="background-color: ' + firewall.getColorForName(n.fwzone) + ';">';
 
 				if (out_ifname)
 					ifname = n.netname.toUpperCase() + ': ';